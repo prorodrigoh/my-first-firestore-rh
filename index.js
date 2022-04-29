d0f06c7d07b6971741cb438df55adc1edf6f279c
ROD <104014121+prorodrigoh@users.noreply.github.com>
2022-04-29 11:47:05 -0400

    Add third entry with replacing db.collection by a variable

--- index.js
+++ index.js
@@ -10,18 +10,18 @@ admin.initializeApp({
 //connect to the firestore project
 const db = admin.firestore();   
 
-// const restaurant = {
-//     name: 'Mister 01',
-//     address: '555 N Federal Hwy Suite #5, Boca Raton, FL 33432',
-//     cuisine: 'Pizza',
-//     rating: 4.9,
-//     phone: '(786) 677-2903',
-// }
+const restaurant = {
+    name: 'Mister 01',
+    address: '555 N Federal Hwy Suite #5, Boca Raton, FL 33432',
+    cuisine: 'Pizza',
+    rating: 4.9,
+    phone: '(786) 677-2903',
+}
 
-// db.collection('restaurants').add(restaurant)
-//     // doc.id is a random string auto generated
-//     .then(doc => console.log('Created restaurant', doc.id))
-//     .catch(err => console.error(err))
+db.collection('restaurants').add(restaurant)
+    // doc.id is a random string auto generated
+    .then(doc => console.log('Created restaurant', doc.id))
+    .catch(err => console.error(err))
 
 const restaurant2 = {
     name: 'Bolay',
@@ -40,3 +40,17 @@ async function addRestaurant (data) {
 }
 addRestaurant(restaurant2)
 
+    // instead of using db.collection('restaurants') everywhere, add it to a variable and use the variable
+    // const restaurants = db.collection('restaurants')
+
+const restaurant3 = {
+    name: 'Addiction Kitchen',
+    address: '2131 NE 48th St, Lighthouse Point, FL 33064',
+    cuisine: 'American',
+    rating: 4.8,
+    phone: '(954) 782-4238',
+}
+
+restaurants.add(restaurant3)
+    .then(doc => console.log('Created restaurant', doc.id))
+    .catch(err => console.error(err))
