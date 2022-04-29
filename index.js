a55dce639c58720b78feeec673ec064c1a04475e
ROD <104014121+prorodrigoh@users.noreply.github.com>
2022-04-29 11:34:41 -0400

    Add second entry with async instead of .then

--- index.js
+++ index.js
@@ -10,14 +10,33 @@ admin.initializeApp({
 //connect to the firestore project
 const db = admin.firestore();   
 
-const restaurant = {
-    name: 'Mister 01',
-    address: '555 N Federal Hwy Suite #5, Boca Raton, FL 33432',
-    cuisine: 'Pizza',
-    rating: 4.9,
-    phone: '(786) 677-2903',
+// const restaurant = {
+//     name: 'Mister 01',
+//     address: '555 N Federal Hwy Suite #5, Boca Raton, FL 33432',
+//     cuisine: 'Pizza',
+//     rating: 4.9,
+//     phone: '(786) 677-2903',
+// }
+
+// db.collection('restaurants').add(restaurant)
+//     // doc.id is a random string auto generated
+//     .then(doc => console.log('Created restaurant', doc.id))
+//     .catch(err => console.error(err))
+
+const restaurant2 = {
+    name: 'Bolay',
+    address: '7060 W Palmetto Park Rd, Boca Raton, FL 33433',
+    cuisine: 'American',
+    rating: 4.6,
 }
 
-db.collection('restaurants').add(restaurant)
-    .then(doc => console.log('Created restaurant', doc.id))
-    .catch(err => console.error(err))
+async function addRestaurant (data) {
+    try {
+        const doc = await db.collection('restaurants').add(data)
+        console.log('Created restaurant', doc.id)
+    } catch(err) {
+        console.error(err)
+    }
+}
+addRestaurant(restaurant2)
+
