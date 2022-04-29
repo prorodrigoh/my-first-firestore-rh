8a3c5d3a9ae58312cfb23d105e4998e16f0d9411
ROD <104014121+prorodrigoh@users.noreply.github.com>
2022-04-29 14:57:24 -0400

    DRY by creating connectDb.js and replacing in the files

--- index.js
+++ index.js
@@ -7,17 +7,19 @@
 //   credential: admin.credential.cert(creds)
 // });
 
-// connect to firebase suite via ES6 - use import instead of require and CHANGE package.json to add "type": "module"
-import admin from 'firebase-admin'
-import creds from './credentials.js' // get our key that we got from firestore database to allow a connection to it
+// // connect to firebase suite via ES6 - use import instead of require and CHANGE package.json to add "type": "module"
+// import admin from 'firebase-admin'
+// import creds from './credentials.js' // get our key that we got from firestore database to allow a connection to it
 
-admin.initializeApp({ //connect to the firebase suite
-  credential: admin.credential.cert(creds) // creates a certificate from our credentials
-});
+// admin.initializeApp({ //connect to the firebase suite
+//   credential: admin.credential.cert(creds) // creates a certificate from our credentials
+// });
 
-const db = admin.firestore(); // connect to the firestore database and create a variable to use
+// const db = admin.firestore(); // connect to the firestore database and create a variable to use
 
-const restaurants = db.collection('restaurants') // instead of using db.collection('restaurants') everywhere, add it to a variable and use the variable
+// const restaurants = db.collection('restaurants') // instead of using db.collection('restaurants') everywhere, add it to a variable and use the variable
+
+import {restaurants} from './connectDb.js'
 
 const restaurant = {
     name: 'Mister 01',
